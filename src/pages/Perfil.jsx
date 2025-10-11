4cd249e83c916655bfa3ef280ebdfb0172a99fd7
Kauã Henrique <kauahenriquesx09@gmail.com>
2025-10-11 17:16:50 -0300

    Front end web completos

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import api from '../services/api';
 import Swal from 'sweetalert2';
-import './Perfil.css';
+import './perfil.css';
 
 function Perfil() {
   const { user, token, logout } = useAuth();
