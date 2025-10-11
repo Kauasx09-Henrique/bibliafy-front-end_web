537ee3608e326fe48eaf7c59fe4d9dbfc9ed58cb
Kauã Henrique <kauahenriquesx09@gmail.com>
2025-10-11 17:28:47 -0300

    Corrige import e adiciona Perfil.css

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import api from '../services/api';
 import Swal from 'sweetalert2';
-import './perfil.css';
+import './Perfil.css';
 
 
 
