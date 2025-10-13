a58e710f55fcdb469c26babcf812927ed4ff4329
Kauã Henrique <kauaheenriquesx09@gmail.com>
2025-10-13 13:51:32 -0300

    erro servidor arrumado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
+// 1. Importa a biblioteca Axios para o seu arquivo.
 import axios from 'axios';
 
+// 2. Cria uma nova instância do Axios com configurações personalizadas.
 const api = axios.create({
-  baseURL: 'https://bibliafy-api.onrender.com', // A URL base do seu backend
+  // 3. Define a URL base para todas as chamadas feitas com esta instância.
+  baseURL: 'https://bibliafy-api.onrender.com',
 });
 
+// 4. Exporta a instância 'api' para que outros arquivos no seu projeto possam usá-la.
 export default api;
